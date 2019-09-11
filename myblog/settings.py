755ce3e0fbb8071a138fe37f84a3c811c408187e
pangzi.xiao <823770059@qq.com>
2019-09-11 10:28:29 +0800

    update   navbar

--- myblog/settings.py
+++ myblog/settings.py
@@ -152,7 +152,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'/static/')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 #媒体文件
 MEDIA_URL = '/media/'
@@ -207,6 +211,3 @@ CKEDITOR_CONFIGS = {
     }
 }
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
