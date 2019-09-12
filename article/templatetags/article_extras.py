102fddcad4e27dee95b3bdc9ee25bca79e2a657e
pangzi.xiao <823770059@qq.com>
2019-09-12 11:00:36 +0800

    update

--- article/templatetags/article_extras.py
+++ article/templatetags/article_extras.py
@@ -1,5 +1,7 @@
-from ..models import ArticlePost,Timeline
+from ..models import ArticlePost
 from  django import template
+from django.utils import timezone
+import math
 
 register = template.Library()
 
@@ -9,3 +11,28 @@ def show_recent_articles(context, num=5):
     return {
         'recent_article_list': ArticlePost.objects.all().order_by('-created')[:num],
     }
+
+
+#获取相对时间
+@register.filter(name='timesince_zh')
+def time_since_zh(value):
+    now = timezone.now()
+    diff = now - value
+
+    if diff.days == 0 and diff.seconds >= 0 and diff.seconds < 60:
+        return '刚刚'
+
+    if diff.days == 0 and diff.seconds>= 60 and diff.seconds < 3600 :
+        return str(math.floor(diff.seconds / 60)) + '分钟前'
+
+    if diff.days == 0 and diff.seconds >= 3600 and diff.seconds <86400:
+        return str(math.floor(diff.seconds / 3600)) + '小时前'
+
+    if diff.days >= 1 and diff.days< 30 :
+        return str(diff.days) + '天前'
+
+    if diff.days >= 30 and diff.days < 365:
+        return str(math.floor(diff.days / 30)) + '个月前'
+
+    if diff.dasy >= 365:
+        return str(math.floor(diff.days /365)) + '年前'
