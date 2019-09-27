9dd04496ea6bb7eb1e1775a5bfda3116b0e25f47
pangzi.xiao <823770059@qq.com>
2019-09-27 13:38:39 +0800

    新增多级评论

--- userprofile/admin.py
+++ userprofile/admin.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
 from .models import Profile
-import xadmin
 # Register your models here.
 
 class ProfileInline(admin.StackedInline):
